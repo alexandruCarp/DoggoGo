70b4d743e25220d021a795b3b352713b6eb2579d
Alexandru Carp <alexandrucarp02@gmail.com>
2022-12-30 02:07:10 +0200

    time of upload + latest discoveries feature

--- home.py
+++ home.py
@@ -1,8 +1,26 @@
 from flask import Blueprint
 from flask import render_template
+from db import get_db
 
 bp = Blueprint("home",__name__)
 
 @bp.route("/")
 def home():
-    return render_template("home.html")
+    db = get_db()
+    db.commit()
+    discoveries = db.execute(
+        "SELECT *"
+        " FROM dog"
+        " ORDER BY discovered DESC"
+        " LIMIT 3"
+    ).fetchall()
+    users={}
+    for disc in discoveries:
+        users[disc['user_id']] = db.execute(
+            " SELECT username "
+            " FROM user "
+            " WHERE id = ? ",
+            (disc['user_id'],)
+        ).fetchall()[0]
+
+    return render_template("home.html", discoveries=discoveries, users=users)
--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE dog (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     breed TEXT NOT NULL,
     photo_path TEXT NOT NULL,
-    discovered TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    discovered TIMESTAMP NOT NULL DEFAULT (datetime('now', 'localtime')),
     user_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id)
 
